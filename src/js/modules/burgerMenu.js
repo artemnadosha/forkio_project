3f23d9d028479584e67ccfdbf93d1ff73be209bb
unidash <egorosinsky@gmail.com>
2023-01-25 23:35:01 +0200

    header and hero-header sections done

--- src/js/modules/burgerMenu.js
+++ src/js/modules/burgerMenu.js
@@ -5,3 +5,13 @@ burgerBtn.addEventListener("click", () => {
   burgerBtn.classList.toggle("active");
   burgerList.classList.toggle("active");
 });
+
+document.body.addEventListener('click', (e) => {
+  if (burgerBtn.classList.contains('active') && e.target.closest('div') !== burgerBtn) {
+
+    if (!e.target.closest('ul')){
+      burgerBtn.classList.toggle("active");
+      burgerList.classList.toggle("active");
+    };
+  };
+});
